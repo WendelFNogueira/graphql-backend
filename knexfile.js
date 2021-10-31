5f1732363493f9d2aae48a1551102f4e1de45ab5
WendelNogueira23 <wendel.felipe89@gmail.com>
2021-10-31 20:13:17 -0300

    condição de acesso ao port para o heroku setar dinamicamente

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
   client: process.env.DB_CLIENT,
   connection: {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.PORT || process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
